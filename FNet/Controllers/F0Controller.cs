d3d57d539e6250f4a6499795cb0b6a3a980f522b
Sokolov <sokolov_ea@farmsib.ru>
2018-11-11 20:51:35 +0300

    F0Controller

---
+++ FNet/Controllers/F0Controller.cs
@@ -0,0 +1,23 @@
+﻿using MvcApplication2.Areas.Settings.Models;
+using Nskd;
+using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Areas.Settings.Controllers
+{
+    public class F0Controller : Controller
+    {
+        public ActionResult Index(Guid sessionId)
+        {
+            String settingsDsAsJsonString = F0Model.Get(sessionId);
+            return PartialView("~/Areas/Settings/Views/F0/Index.cshtml", settingsDsAsJsonString);
+        }
+        public Object Save()
+        {
+            RequestPackage rqp = RequestPackage.ParseRequest(Request.InputStream, Request.ContentEncoding);
+            String settingsDsAsJsonString = F0Model.Set(rqp);
+            return settingsDsAsJsonString;
+        }
+    }
+}
